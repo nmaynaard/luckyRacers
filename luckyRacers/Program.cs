5c6b9df937e9a5430d3c3f9902fe1c59bdf65a26
Nathan Maynard <nathanmaynard@MacBook-Pro.local>
2022-09-30 22:16:37 +0100

    Uploaded project.

---
+++ luckyRacers/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Reflection;
+using static System.Collections.Specialized.BitVector32;
+
+namespace luckyRacers
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Car car01 = new Car("Ford", "Fiesta");
+            Line();
+            Console.WriteLine("                  Lucky Racers");
+            Console.WriteLine("               By Nathan Maynard");
+            Console.WriteLine("        Current Time: " + DateTime.Now);
+            Line();
+            Console.WriteLine("");
+            Console.WriteLine("Hit ENTER to Continue");
+            Console.ReadKey();
+            creation();
+            menu(car01);
+
+
+        }
+
+
+        public static object createCar(string make, string model)
+        {
+            Car car01 = new Car(make, model);
+            return car01;
+
+        }
+
+        static void creation()
+        {
+            Console.Clear();
+            Console.WriteLine("Please Select a Beginner Car From Below:");
+            Line();
+            Console.WriteLine("Ford Fiesta");
+            Console.WriteLine("Citroen C1");
+            Console.WriteLine("Vauxhall Corsa");
+            Line();
+            Console.WriteLine("Selection: ");
+            string selection = Console.ReadLine();
+            if (selection == "1")
+            {
+                createCar("Ford", "Fiesta");
+                Console.WriteLine("You Have chosen Ford Fiesta");
+                Console.ReadKey();
+
+            }
+            else if (selection == "2")
+            {
+                createCar("Citroen", "C1");
+                Console.WriteLine("You've chosen Citroen C1");
+                Console.ReadKey();
+
+            }
+            else if (selection == "3")
+            {
+                createCar("Vauxhall", "Corsa");
+                Console.WriteLine("You've chosen Vauxhall Corsa");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Value!");
+                Console.WriteLine("Press any key to retry");
+                Console.ReadKey();
+                creation();
+            }
+        }
+
+        public static void Line()
+        {
+            int i;
+            for (i = 0; i < 50; i++)
+            {
+                Console.Write("-");
+
+            }
+            Console.WriteLine("");
+        }
+
+
+        static void menu(Car car01)
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("      _.-.___\\__");
+            Console.WriteLine("      |  _      _`-.      LUCKY RACERS");
+            Console.WriteLine("      '-(_)----(_)--`   Car: {0} {1}", car01.make, car01.model);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("      Experience: {0}           Money: £{1}", car01.experience, car01.money);
+            Console.WriteLine("");
+            Line();
+            Console.WriteLine("                    MAIN MENU");
+            Console.WriteLine("                      Race");
+            Console.WriteLine("                      [WIP] Upgrade");
+            Console.WriteLine("                      Refuel({0})", car01.fuel);
+            Console.WriteLine("                      [WIP] Garage");
+            Console.WriteLine("                      [WIP] Marketplace");
+            Console.WriteLine("                      Stats");
+            Console.WriteLine("                      [WIP] Save & Exit");
+            Line();
+            Console.WriteLine("Select Option: ");
+            string menuSelect = Console.ReadLine();
+            if (menuSelect == "1")
+            {
+
+                car01.Race();
+                Console.ReadLine();
+                menu(car01);
+            }
+            else if (menuSelect == "6")
+            {
+                Console.Clear();
+                Line();
+                Console.WriteLine("Experience: " + car01.experience);
+                Console.WriteLine("Money: £" + car01.money);
+                Console.WriteLine("Wins: " + car01.wins);
+                Console.ReadLine();
+                menu(car01);
+            }
+            else if (menuSelect == "3")
+            {
+                car01.Refuel();
+                Console.ReadLine();
+                menu(car01);
+            }
+            else
+            {
+                menu(car01);
+            }
+        }
+    }
+}
---
+++ luckyRacers/car.cs
@@ -0,0 +1,152 @@
+﻿using System;
+namespace luckyRacers
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int horsepower;
+        public float experience;
+        public int fuel;
+        public int money;
+        public int wins;
+
+
+        public Car(string _make, string _model)
+        {
+            make = _make;
+            model = _model;
+            fuel = 5;
+            experience = 0;
+            horsepower = 100;
+            money = 100;
+
+        }
+
+        public void Race()
+        {
+            if (fuel > 0)
+            {
+                Console.Clear();
+                Program.Line();
+                fuel--;
+                Random rand = new Random();
+                int number = rand.Next(1, 101);
+                if (number > 40)
+                {
+                    Console.WriteLine("You Finished 1st");
+                    wins++;
+                    Console.WriteLine("Hit ENTER for Rewards!");
+                    Console.WriteLine("You have used fuel and have {0} remaining", fuel);
+                    Program.Line();
+                    Console.ReadKey();
+                    Reward();
+
+                }
+                else if (number > 25)
+                {
+                    Console.WriteLine("You Finished 2nd");
+                    Console.WriteLine("Hit ENTER for Rewards!");
+                    Console.WriteLine("You have used fuel and have {0} remaining", fuel);
+                    Program.Line();
+                    Console.ReadKey();
+                    Reward();
+
+                }
+                else if (number > 20)
+                {
+                    Console.WriteLine("You Finished 3rd");
+                    Console.WriteLine("Hit ENTER for Rewards!");
+                    Program.Line();
+                    Console.WriteLine("You have used fuel and have {0} remaining", fuel);
+                    Console.ReadKey();
+                    Reward();
+
+                }
+                else if (number < 20)
+                {
+
+                    Console.WriteLine("You Finished 4th");
+                    Console.WriteLine("Hit ENTER for Rewards!");
+                    Program.Line();
+                    Console.WriteLine("You have used fuel and have {0} remaining", fuel);
+                    Console.ReadKey();
+                    Reward();
+                }
+                else if (number < 15)
+                {
+
+                    Console.WriteLine("You Finished 5th");
+                    Console.WriteLine("Hit ENTER for Rewards!");
+                    Program.Line();
+                    Console.WriteLine("You have used fuel and have {0} remaining", fuel);
+                    Console.ReadKey();
+                    Reward();
+                }
+                else if (number < 10)
+                {
+
+                    Console.WriteLine("You Finished Lower than 5th");
+                    Console.WriteLine("Not Eligible for Rewards!");
+                    Console.WriteLine("You have used fuel and have {0} remaining", fuel);
+                    Program.Line();
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unable to Race your car is out of fuel!");
+            }
+
+        }
+
+        public void Refuel()
+        {
+            fuel += 5;
+            money -= 32500;
+            Console.WriteLine("You've Spent £32500 Refuelling");
+            Console.WriteLine("Your {0} {1} refuels and you now have {2} fuel", make, model, fuel);
+
+        }
+        private void Reward()
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+            Program.Line();
+            if (number > 90)
+            {
+                experience += 12.5f;
+                money += 25000;
+                Console.WriteLine("Tier 1 Rewards!");
+                Console.WriteLine("You've Gained 12.27 experience and £25000");
+                Program.Line();
+
+            }
+            else if (number > 50)
+            {
+                experience += 6f;
+                money += 12500;
+                Console.WriteLine("Tier 2 Rewards!");
+                Console.WriteLine("You've Gained 6.12 experience and £12500");
+                Program.Line();
+
+            }
+            else if (number > 25)
+            {
+                experience += 3.75f;
+                money += 6500;
+                Console.WriteLine("Tier 3 Rewards!");
+                Console.WriteLine("You've Gained 3.12 experience and £6500");
+                Program.Line();
+
+            }
+            else if (number < 25)
+            {
+
+                Console.WriteLine("You Did Not Win Anything This Time!");
+                Program.Line();
+            }
+
+        }
+    }
+}
